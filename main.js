ad4b3a283b53e06c87e34676cc52f685d379e832
William Kowalski <williamkowalski@gmail.com>
2024-02-22 17:12:54 -0400

    Update main.js
    Reset

--- main.js
+++ main.js
@@ -1,34 +1,59 @@
 const moon = document.getElementById('moon');
 const planet = document.getElementById('planet');
-function getXOf(body) { // use this to get position of moon and planet
+
+function getPosition(body) { // use this to get position of moon and planet
     const rect = body.getBoundingClientRect();
     const x = rect.left;
+    const y = rect.top;
     return (x);
 }
-let moonX = getXOf(moon);
-let planetX = getXOf(planet);
-let distance = planetX - moonX;
+
+
+let moonPos = getPosition(moon);
+let planetPos = getPosition(planet);
+let distance = planetPos - moonPos;
+
+let stopAnimation = false;
 let styles = window.getComputedStyle(moon); // figure out where moon is at any moment
+
+// make text appear on the water (#reflection)
+setTimeout(function(){
+    document.getElementById('reflection').style.opacity = "1";
+    document.getElementById('mahone').style.display = "block";
+    document.getElementById('bay').style.display = "block";
+    document.getElementById('mbwd-logo').style.display = "block";
+    document.getElementById('web').style.display = "block";
+    document.getElementById('design').style.display = "block";
+}, 4000)
+
+
 let i = 0;
-function animateMoon() {
-    if(true){
-        currentX = Math.floor(getXOf(moon));
+function animate() {
+    if(!stopAnimation){
+        
+        currentX = Math.floor(getPosition(moon));
+
         if (i === 0){
-            startingX = getXOf(moon); // get value of X at beginning of orbit
+            startingX = getPosition(moon); // get value of X at beginning of orbit
         }
         i++;
+        
         requestAnimationFrame(animate);
-        let moonX = getXOf(moon);
-        let planetX = getXOf(planet);
-        let distance = Math.floor(planetX - moonX);
+        let moonPos = getPosition(moon);
+        let planetPos = getPosition(planet);
+        let distance = Math.floor(planetPos - moonPos);
         console.log(distance);
+
         if (distance > 150){
             moon.style.zIndex = '10';
         }
+       
         if (distance < -175 ) {
             moon.style.zIndex = '-10';
         }
+       
     }
+    
 }
-animateMoon();
 
+animate();
